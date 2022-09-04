196bb52bc3d2111800e4501a025b4d04bee4e1ed
FordRelak <fordrelak.4050@yandex.ru>
2022-09-04 18:28:34 +0300

    #16 Implement error handling

---
+++ src/Application/StreamRoom.Application.GraphQL/Filters/AppErrorFilter.cs
@@ -0,0 +1,21 @@
+﻿using HotChocolate;
+using StreamRoom.Application.GraphQL.Exceptions;
+
+namespace StreamRoom.Application.GraphQL.Filters;
+
+public class AppErrorFilter : IErrorFilter
+{
+    public IError OnError(IError error)
+    {
+        if(error.Exception is AppException appException)
+        {
+            error = error
+                .WithCode(appException.Code)
+                .WithMessage(appException.Message)
+                .RemoveExtensions()
+                .RemoveLocations();
+        }
+
+        return error;
+    }
+}
