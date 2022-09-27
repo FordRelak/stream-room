57cda9923e9bc1aef0ec7480e8439b2887e3d122
FordRelak <fordrelak.4050@yandex.ru>
2022-09-27 17:44:14 +0300

    #33 Remove user from room

---
+++ src/Application/StreamRoom.Application.GraphQL/Mutations/RemoveUserFromRoom/RemoveUserFromRoomInput.cs
@@ -0,0 +1,2 @@
+﻿namespace StreamRoom.Application.GraphQL.Mutations.RemoveUserFromRoom;
+public record RemoveUserFromRoomInput(Guid UserId, Guid RoomId);
---
+++ src/Application/StreamRoom.Application.GraphQL/Mutations/RemoveUserFromRoom/RemoveUserFromRoomPayload.cs
@@ -0,0 +1,10 @@
+﻿namespace StreamRoom.Application.GraphQL.Mutations.RemoveUserFromRoom;
+public class RemoveUserFromRoomPayload : Payload
+{
+    public Guid RoomId { get; set; }
+
+    public RemoveUserFromRoomPayload(Guid roomId)
+    {
+        RoomId = roomId;
+    }
+}
